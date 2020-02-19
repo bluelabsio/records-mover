3c7d0a0c45fbbcb369694084a840830eb175c83e
Vince Broz <vince.broz@bluelabs.com>
2020-02-19 17:00:39 -0500

    Handle BigQuery creds in Airflow

--- records_mover/creds/creds_via_airflow.py
+++ records_mover/creds/creds_via_airflow.py
@@ -2,7 +2,7 @@ from .base_creds import BaseCreds
 import boto3
 import logging
 from db_facts.db_facts_types import DBFacts
-from typing import Iterable, TYPE_CHECKING
+from typing import Iterable, Optional, Union, TYPE_CHECKING
 if TYPE_CHECKING:
     # see the 'gsheets' extras_require option in setup.py - needed for this!
     import google.auth.credentials  # noqa
@@ -19,14 +19,26 @@ class CredsViaAirflow(BaseCreds):
     def db_facts(self, db_creds_name: str) -> DBFacts:
         from airflow.hooks import BaseHook
         conn = BaseHook.get_connection(db_creds_name)
-        return {
-            'host': conn.host,
-            'port': conn.port,
-            'database': conn.schema,
-            'user': conn.login,
-            'password': conn.password,
-            'type': conn.extra_dejson.get('type', conn.conn_type.lower()),
-        }
+        out: DBFacts = {}
+
+        def add(key: str, value: Optional[Union[str, int]]) -> None:
+            if value is not None:
+                out[key] = value  # type: ignore
+
+        add('port', conn.port)
+        add('database', conn.schema)
+        add('user', conn.login)
+        add('password', conn.password)
+        # conn.extra_dejson returns {} if no 'extra' is set in the Connection:
+        # https://airflow.apache.org/docs/stable/_modules/airflow/models/connection.html
+        add('type', conn.extra_dejson.get('type', conn.conn_type.lower()))
+        add('bq_default_project_id', conn.extra_dejson.get('extra__google_cloud_platform__project'))
+        add('bq_default_dataset_id', conn.extra_dejson.get('bq_default_dataset_id'))
+        add('bq_service_account_json',
+            conn.extra_dejson.get('extra__google_cloud_platform__keyfile_dict'))
+        add('protocol', conn.extra_dejson.get('protocol'))
+
+        return out
 
     def _gcp_creds(self, gcp_creds_name: str,
                    scopes: Iterable[str]) -> 'google.auth.credentials.Credentials':
