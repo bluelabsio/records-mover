992d0171633a01a5002ee66923a1bf07e9742da9
vinceatbluelabs <vince.broz@bluelabs.com>
2020-02-20 14:53:52 -0500

    Create BigQuery service account db-facts from Airflow connections (#5)
    * Improve type coverage
    
    * Handle BigQuery creds in Airflow
    
    * Adjust existing creds_via_airflow test around detail of new code
    
    * Add new test to cover BigQuery service accounts via Airflow conns

--- tests/unit/creds/test_creds_via_airflow.py
+++ tests/unit/creds/test_creds_via_airflow.py
@@ -17,19 +17,48 @@ class TestCredsViaAirflow(unittest.TestCase):
         self.assertEqual(mock_AwsHook.return_value.get_session.return_value, out)
 
     @patch('airflow.hooks.BaseHook')
-    def test_db_facts(self, mock_BaseHook):
+    def test_db_facts_normcoredb(self, mock_BaseHook):
         mock_db_creds_name = Mock(name='db_creds_name')
+        mock_conn = mock_BaseHook.get_connection.return_value
+        mock_conn.extra_dejson = {
+            'type': 'jsontype'
+        }
         out = self.creds_via_airflow.db_facts(mock_db_creds_name)
         mock_BaseHook.get_connection.assert_called_with(mock_db_creds_name)
-        mock_conn = mock_BaseHook.get_connection.return_value
         expected_db_facts = {
             'host': mock_conn.host,
             'port': mock_conn.port,
             'database': mock_conn.schema,
             'password': mock_conn.password,
-            'type': mock_conn.extra_dejson.get.return_value,
+            'type': 'jsontype',
             'user': mock_conn.login
         }
-        mock_conn.extra_dejson.get.assert_called_with('type',
-                                                      mock_conn.conn_type.lower.return_value)
+        self.assertEqual(expected_db_facts, out)
+
+    @patch('airflow.hooks.BaseHook')
+    def test_db_facts_bigquery_serviceaccount(self, mock_BaseHook):
+        mock_db_creds_name = Mock(name='db_creds_name')
+        mock_conn = mock_BaseHook.get_connection.return_value
+        mock_conn.type = 'google_cloud_platform'
+        mock_conn.schema = None
+        mock_conn.host = None
+        mock_conn.port = None
+        mock_conn.password = None
+        mock_conn.login = None
+        mock_conn.extra_dejson = {
+            'type': 'bigquery',
+            'bq_default_dataset_id': 'mydataset',
+            'protocol': 'bigquery',
+            'extra__google_cloud_platform__keyfile_dict': 'big json block',
+            'extra__google_cloud_platform__project': 'myproject',
+        }
+        out = self.creds_via_airflow.db_facts(mock_db_creds_name)
+        mock_BaseHook.get_connection.assert_called_with(mock_db_creds_name)
+        expected_db_facts = {
+            'bq_default_dataset_id': 'mydataset',
+            'bq_default_project_id': 'myproject',
+            'bq_service_account_json': 'big json block',
+            'protocol': 'bigquery',
+            'type': 'bigquery',
+        }
         self.assertEqual(expected_db_facts, out)
