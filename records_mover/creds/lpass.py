6290bcaf99b3b5492950f12b5abbabfde84196c2
Vince Broz <vince.broz@bluelabs.com>
2020-02-22 10:33:26 -0500

    Remove deprecated lpass_show()

--- records_mover/creds/lpass.py
+++ records_mover/creds/lpass.py
@@ -1,36 +1,6 @@
 from subprocess import check_output
 from records_mover.db.db_type import canonicalize_db_type, db_protocol
-from ..utils import deprecated
 from db_facts.db_facts_types import DBFacts
-
-
-# This function was written under Python 2.7 and never got fixed to be
-# Python 3 compatible, so it kept returning a byte array when it
-# should have been returning a string.  It also never chomped the last
-# newline off of the command line output, so the API to it was never
-# great.  Meanwhile, clients of it started to use it and then expect
-# the results to be byte arrays, so just changing behavior isn't a
-# great option regardless.
-#
-# So, let's deprecate it until the next major version here.
-#
-# Please switch code over to use lpass_field instead.
-@deprecated
-def lpass_show(name: str, field: str) -> bytes:
-    if field == 'notes':
-        field_arg = '--notes'
-    elif field == 'username':
-        field_arg = '--username'
-    elif field == 'password':
-        field_arg = '--password'
-    elif field == 'url':
-        field_arg = '--url'
-    else:
-        field_arg = '--field=' + field
-    return check_output(['lpass',
-                         'show',
-                         field_arg,
-                         name])
 
 
 def lpass_field(name: str, field: str) -> str:
