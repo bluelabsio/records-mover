e0746501ba2e38959e2b54e82038e5d4d8ef305a
Vince Broz <vince.broz@bluelabs.com>
2020-02-22 19:35:27 -0500

    Add a missing import found by tests

--- records_mover/records/schema/field/representation.py
+++ records_mover/records/schema/field/representation.py
@@ -55,6 +55,8 @@ class RecordsSchemaFieldRepresentation(metaclass=ABCMeta):
 
     @staticmethod
     def from_series(series: 'pandas.Series') -> 'RecordsSchemaFieldRepresentation':
+        import pandas
+
         dtype_json_str = pandas.io.json.dumps(series.dtype)
         dtype_numpy_rep = json.loads(dtype_json_str)
         ftype = getattr(series, 'ftype', None)
