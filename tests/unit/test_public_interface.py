d7d0bea609afa0eef0877673edd4410b836c734a
Vince Broz <vince.broz@bluelabs.com>
2020-03-02 18:42:17 -0500

    Add a test of public interface from internal uses

---
+++ tests/unit/test_public_interface.py
@@ -0,0 +1,5 @@
+from records_mover.airflow.hooks import records_hook  # noqa
+from records_mover import Session  # noqa
+from records_mover.records import ExistingTableHandling  # noqa
+from records_mover.records import DelimitedRecordsFormat  # noqa
+from records_mover.records import RecordsFormat  # noqa
