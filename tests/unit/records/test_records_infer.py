b649c5d8d6f04ccb59e247523cc94e7686891ca6
Vince Broz <vince.broz@bluelabs.com>
2020-03-02 20:28:28 -0500

    Resolve merge conflicts

--- tests/unit/records/test_records_infer.py
+++ tests/unit/records/test_records_infer.py
@@ -6,8 +6,18 @@ from mock import patch
 class TestRecordsInfer(unittest.TestCase):
     @patch('records_mover.Session')
     @patch('records_mover.records.records.Records')
-    def test_record_infer(self, mock_Records, mock_Session):
+    @patch('records_mover.records.records.RecordsSources')
+    @patch('records_mover.records.records.RecordsTargets')
+    def test_record_infer(self,
+                          mock_RecordsTargets,
+                          mock_RecordsSources,
+                          mock_Records,
+                          mock_Session):
         mock_session = mock_Session.return_value
         records = Records()
-        self.assertEqual(records.db_driver, mock_session.db_driver)
-        self.assertEqual(records.url_resolver, mock_session.url_resolver)
+        self.assertEqual(records.sources, mock_RecordsSources.return_value)
+        self.assertEqual(records.targets, mock_RecordsTargets.return_value)
+        mock_RecordsTargets.assert_called_with(db_driver=mock_session.db_driver,
+                                               url_resolver=mock_session.url_resolver)
+        mock_RecordsSources.assert_called_with(db_driver=mock_session.db_driver,
+                                               url_resolver=mock_session.url_resolver)
