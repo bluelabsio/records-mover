972c96463a557ebc3c75c8980ff340867019e35a
Vince Broz <vince.broz@bluelabs.com>
2020-03-13 19:53:51 -0400

    Increase coverage

--- tests/unit/db/postgres/test_date_input_style.py
+++ tests/unit/db/postgres/test_date_input_style.py
@@ -7,7 +7,6 @@ class TestDateInputStyle(unittest.TestCase):
     def test_determine_date_input_style_(self):
         unhandled_hints = set()
         tests = [
-
             (
                 # No ambiguity, can handle all
                 {
@@ -17,11 +16,27 @@ class TestDateInputStyle(unittest.TestCase):
                     'dateformat': "YYYY-MM-DD",
                 },
                 None
-            )
+            ),
+            (
+                # No ambiguity, can handle all
+                {
+                    'datetimeformattz': 'INVALID',
+                    'datetimeformat': "YYYY-MM-DD HH12:MI AM",
+                    'timeonlyformat': "HH12:MI AM",
+                    'dateformat': "YYYY-MM-DD",
+                },
+                'RAISE'
+            ),
         ]
         fail_if_cant_handle_hint = True
         for hints, expected_result in tests:
-            out = determine_date_input_style(unhandled_hints,
-                                             hints,
-                                             fail_if_cant_handle_hint)
-            self.assertEqual(out, expected_result)
+            if expected_result == 'RAISE':
+                with self.assertRaises(NotImplementedError):
+                    determine_date_input_style(unhandled_hints,
+                                               hints,
+                                               fail_if_cant_handle_hint)
+            else:
+                out = determine_date_input_style(unhandled_hints,
+                                                 hints,
+                                                 fail_if_cant_handle_hint)
+                self.assertEqual(out, expected_result)
