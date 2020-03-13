ef8074b8c979520916926c566284778a084b9546
Vince Broz <vince.broz@bluelabs.com>
2020-03-13 19:43:22 -0400

    Increase coverage

--- tests/unit/db/postgres/test_loader.py
+++ tests/unit/db/postgres/test_loader.py
@@ -72,6 +72,59 @@ class TestPostgresLoader(unittest.TestCase):
     @patch('records_mover.db.postgres.loader.complain_on_unhandled_hints')
     @patch('records_mover.db.postgres.loader.Table')
     @patch('records_mover.db.postgres.loader.postgres_copy_options')
+    def test_load_from_fileobj_default_date_input_style(self,
+                                                        mock_postgres_copy_options,
+                                                        mock_Table,
+                                                        mock_complain_on_unhandled_hints,
+                                                        mock_copy_from,
+                                                        mock_quote_value):
+        mock_schema = Mock(name='schema')
+        mock_table = Mock(name='table')
+        mock_load_plan = Mock(name='load_plan')
+        mock_fileobj = Mock(name='fileobj')
+
+        mock_records_format = Mock(name='records_format',
+                                   spec=DelimitedRecordsFormat)
+        mock_records_format.hints = {}
+        mock_load_plan.records_format = mock_records_format
+        mock_date_input_style = None
+        mock_postgres_options = {
+            'abc': 123
+        }
+        mock_postgres_copy_options.return_value = (
+            mock_date_input_style,
+            mock_postgres_options,
+        )
+        mock_quote_value.return_value = "ABC"
+        self.loader.load_from_fileobj(mock_schema,
+                                      mock_table,
+                                      mock_load_plan,
+                                      mock_fileobj)
+        mock_processing_instructions = mock_load_plan.processing_instructions
+        mock_unhandled_hints = set(mock_records_format.hints.keys())
+        mock_complain_on_unhandled_hints.\
+            assert_called_with(mock_processing_instructions.fail_if_dont_understand,
+                               mock_unhandled_hints,
+                               mock_records_format.hints)
+        mock_table_obj = mock_Table.return_value
+        mock_Table.assert_called_with(mock_table,
+                                      self.mock_meta,
+                                      schema=mock_schema,
+                                      autoload=True,
+                                      autoload_with=self.mock_db)
+        mock_conn = self.mock_db.engine.begin.return_value.__enter__.return_value
+        mock_quote_value.assert_called_with(mock_conn, 'ISO, MDY')
+        mock_conn.execute.assert_called_with('SET LOCAL DateStyle = ABC')
+        mock_copy_from.assert_called_with(mock_fileobj,
+                                          mock_table_obj,
+                                          mock_conn,
+                                          abc=123)
+
+    @patch('records_mover.db.postgres.loader.quote_value')
+    @patch('records_mover.db.postgres.loader.copy_from')
+    @patch('records_mover.db.postgres.loader.complain_on_unhandled_hints')
+    @patch('records_mover.db.postgres.loader.Table')
+    @patch('records_mover.db.postgres.loader.postgres_copy_options')
     def test_load(self,
                   mock_postgres_copy_options,
                   mock_Table,
@@ -130,3 +183,14 @@ class TestPostgresLoader(unittest.TestCase):
                                           mock_table_obj,
                                           mock_conn,
                                           abc=123)
+
+    @patch('records_mover.db.postgres.loader.complain_on_unhandled_hints')
+    @patch('records_mover.db.postgres.loader.postgres_copy_options')
+    def test_can_load_this_format_true(self,
+                                       mock_postgres_copy_options,
+                                       mock_complain_on_unhandled_hints):
+        source_records_format = Mock(name='source_records_format',
+                                     spec=DelimitedRecordsFormat)
+        source_records_format.hints = {}
+        out = self.loader.can_load_this_format(source_records_format)
+        self.assertTrue(out)
