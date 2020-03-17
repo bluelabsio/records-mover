a3dc9ecff2682f610e3ae52384522aecc94ea77e
Vince Broz <vince.broz@bluelabs.com>
2020-03-17 06:20:28 -0400

    Ratchet mypy coverage

--- types/stubs/google/oauth2/service_account/__init__.pyi
+++ types/stubs/google/oauth2/service_account/__init__.pyi
@@ -1,11 +1,11 @@
-from typing import Mapping
+from typing import Mapping, Iterable
 import google.auth.service_account
 
 # https://google-auth.readthedocs.io/en/latest/reference/google.oauth2.service_account.html
 
 
-class Credentials:
+class Credentials(google.auth.credentials.Credentials):
     @classmethod
-    def from_service_account_info(cls, info: Mapping[str, str], **kwargs) ->\
+    def from_service_account_info(cls, info: Mapping[str, str], scopes: Iterable[str], **kwargs) ->\
             google.auth.service_account.Credentials:
         ...
