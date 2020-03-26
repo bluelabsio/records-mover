4768f4efb809e1df262020a7fa7cd881a483103c
Vince Broz <vince.broz@bluelabs.com>
2020-03-26 11:33:23 -0400

    Ratchet mypy coverage

--- records_mover/db/postgres/sqlalchemy_postgres_copy.pyi
+++ records_mover/db/postgres/sqlalchemy_postgres_copy.pyi
@@ -1,2 +1,10 @@
-def copy_from(source, dest, engine_or_conn, columns=(), **flags):
+from typing import Union, IO
+import sqlalchemy
+
+
+def copy_from(source: IO[bytes],
+              dest,
+              engine_or_conn: Union[sqlalchemy.engine.Engine, sqlalchemy.engine.Connection],
+              columns=(),
+              **flags) -> None:
     ...
