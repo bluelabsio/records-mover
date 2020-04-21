c0002178dd0108b7265f6ffb2b33211b3fc37f37
Vince Broz <vince.broz@bluelabs.com>
2020-04-21 15:29:19 -0400

    Add additional export

--- records_mover/records/__init__.py
+++ records_mover/records/__init__.py
@@ -9,10 +9,12 @@ __all__ = [
     'ProcessingInstructions',
     'ExistingTableHandling',
     'Records',
+    'move'
 ]
 
 from .types import RecordsHints, BootstrappingRecordsHints, RecordsFormatType, DelimitedVariant  # noqa
 from .schema import RecordsSchema  # noqa
+from .mover import move
 from .records_format import RecordsFormat, DelimitedRecordsFormat, ParquetRecordsFormat  # noqa
 from .processing_instructions import ProcessingInstructions  # noqa
 from .existing_table_handling import ExistingTableHandling  # noqa
