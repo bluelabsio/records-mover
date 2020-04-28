d9c8211c10b9cb4b404a5a13e47649b0ce469ffe
Vince Broz <vince.broz@bluelabs.com>
2020-04-28 19:04:40 -0400

    Implement LoaderFromRecordsDirectory

--- records_mover/db/mysql/loader.py
+++ records_mover/db/mysql/loader.py
@@ -1,11 +1,12 @@
 import sqlalchemy
+from records_mover.db.loader import LoaderFromRecordsDirectory
 from records_mover.records.load_plan import RecordsLoadPlan
 from records_mover.records.records_directory import RecordsDirectory
 from records_mover.records.records_format import BaseRecordsFormat
-from typing import Union, Optional, List, IO
+from typing import Union, Optional, List
 
 
-class MySQLLoader:
+class MySQLLoader(LoaderFromRecordsDirectory):
     def __init__(self,
                  db: Union[sqlalchemy.engine.Engine, sqlalchemy.engine.Connection],):
         ...
@@ -21,11 +22,4 @@ class MySQLLoader:
         raise NotImplementedError
 
     def known_supported_records_formats_for_load(self) -> List[BaseRecordsFormat]:
-        raise NotImplementedError
-
-    def can_load_from_fileobjs(self) -> bool:
-        raise NotImplementedError
-
-    def load_from_fileobj(self, schema: str, table: str,
-                          load_plan: RecordsLoadPlan, fileobj: IO[bytes]) -> Optional[int]:
         raise NotImplementedError
