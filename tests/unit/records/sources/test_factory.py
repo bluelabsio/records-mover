50d3a6e5203f64d54fd5082e133e05b2c70f1c11
Vince Broz <vince.broz@bluelabs.com>
2020-05-04 22:04:11 -0400

    Fix factory types

--- tests/unit/records/sources/test_factory.py
+++ tests/unit/records/sources/test_factory.py
@@ -15,39 +15,39 @@ class TestFactory(unittest.TestCase):
     def test_dataframe(self, mock_DataframesRecordsSource):
         mock_df = Mock(name='df')
         mock_processing_instructions = Mock(name='processing_instructions')
-        with self.records_sources.\
+        df = self.records_sources.\
                 dataframe(df=mock_df,
-                          processing_instructions=mock_processing_instructions) as df:
-            mock_DataframesRecordsSource.\
-                assert_called_with(dfs=ANY,
-                                   include_index=False,
-                                   records_schema=None,
-                                   processing_instructions=mock_processing_instructions)
-            self.assertEqual(df, mock_DataframesRecordsSource.return_value)
+                          processing_instructions=mock_processing_instructions)
+        mock_DataframesRecordsSource.\
+            assert_called_with(dfs=ANY,
+                               include_index=False,
+                               records_schema=None,
+                               processing_instructions=mock_processing_instructions)
+        self.assertEqual(df, mock_DataframesRecordsSource.return_value)
 
     @patch('records_mover.records.sources.factory.UninferredFileobjsRecordsSource')
     def test_fileobjs(self, mock_UninferredFileobjsSource):
         mock_target_names_to_fileobjs = Mock(name='target_names_to_fileobjs')
         mock_records_schema = Mock(name='records_schema')
-        with self.records_sources.\
-                fileobjs(target_names_to_input_fileobjs=mock_target_names_to_fileobjs,
-                         records_format=None,
-                         records_schema=mock_records_schema):
-            mock_UninferredFileobjsSource.\
-                assert_called_with(initial_hints=None,
-                                   records_format=None,
-                                   records_schema=mock_records_schema,
-                                   target_names_to_input_fileobjs=mock_target_names_to_fileobjs)
+        self.records_sources.\
+            fileobjs(target_names_to_input_fileobjs=mock_target_names_to_fileobjs,
+                     records_format=None,
+                     records_schema=mock_records_schema)
+        mock_UninferredFileobjsSource.\
+            assert_called_with(initial_hints=None,
+                               records_format=None,
+                               records_schema=mock_records_schema,
+                               target_names_to_input_fileobjs=mock_target_names_to_fileobjs)
 
     @patch('records_mover.records.sources.factory.FileobjsSource')
     def test_data_url(self,
                       mock_FileobjsSource):
         mock_input_url = 'foo://host/path'
         mock_records_format = Mock(name='records_format')
-        with self.records_sources.data_url(input_url=mock_input_url,
-                                           records_format=mock_records_format) as out:
-            self.assertEqual(type(out),
-                             DataUrlRecordsSource)
+        out = self.records_sources.data_url(input_url=mock_input_url,
+                                            records_format=mock_records_format)
+        self.assertEqual(type(out),
+                         DataUrlRecordsSource)
 
     @patch('records_mover.records.sources.table.TableRecordsSource')
     def test_table(self,
@@ -55,31 +55,31 @@ class TestFactory(unittest.TestCase):
         mock_schema_name = Mock(name='schema_name')
         mock_table_name = Mock(name='table_name')
         mock_db_engine = Mock(name='db_engine')
-        with self.records_sources.table(schema_name=mock_schema_name,
-                                        table_name=mock_table_name,
-                                        db_engine=mock_db_engine) as out:
-            mock_TableRecordsSource.assert_called_with(schema_name=mock_schema_name,
-                                                       table_name=mock_table_name,
-                                                       driver=self.mock_db_driver.return_value,
-                                                       url_resolver=self.mock_url_resolver)
-            self.assertEqual(out, mock_TableRecordsSource.return_value)
+        out = self.records_sources.table(schema_name=mock_schema_name,
+                                         table_name=mock_table_name,
+                                         db_engine=mock_db_engine)
+        mock_TableRecordsSource.assert_called_with(schema_name=mock_schema_name,
+                                                   table_name=mock_table_name,
+                                                   driver=self.mock_db_driver.return_value,
+                                                   url_resolver=self.mock_url_resolver)
+        self.assertEqual(out, mock_TableRecordsSource.return_value)
 
     @patch('records_mover.records.sources.google_sheets.GoogleSheetsRecordsSource')
     def test_google_sheet(self, mock_GoogleSheetsRecordsSource):
         mock_spreadsheet_id = Mock(name='spreadsheet_id')
         mock_sheet_name_or_range = Mock(name='sheet_name_or_range')
         mock_google_cloud_creds = Mock(name='google_cloud_creds')
-        with self.records_sources.\
+        out = self.records_sources.\
             google_sheet(spreadsheet_id=mock_spreadsheet_id,
                          sheet_name_or_range=mock_sheet_name_or_range,
-                         google_cloud_creds=mock_google_cloud_creds) as out:
-            mock_GoogleSheetsRecordsSource.\
-                assert_called_with(spreadsheet_id=mock_spreadsheet_id,
-                                   sheet_name_or_range=mock_sheet_name_or_range,
-                                   google_cloud_creds=mock_google_cloud_creds,
-                                   header_translator=None,
-                                   out_of_band_column_headers=None)
-            self.assertEqual(out, mock_GoogleSheetsRecordsSource.return_value)
+                         google_cloud_creds=mock_google_cloud_creds)
+        mock_GoogleSheetsRecordsSource.\
+            assert_called_with(spreadsheet_id=mock_spreadsheet_id,
+                               sheet_name_or_range=mock_sheet_name_or_range,
+                               google_cloud_creds=mock_google_cloud_creds,
+                               header_translator=None,
+                               out_of_band_column_headers=None)
+        self.assertEqual(out, mock_GoogleSheetsRecordsSource.return_value)
 
     @patch('records_mover.records.compression.os')
     @patch('records_mover.records.compression.urlparse')
@@ -90,6 +90,6 @@ class TestFactory(unittest.TestCase):
                         mock_os):
         mock_filename = Mock(name='filename')
         mock_records_format = Mock(name='record_format')
-        with self.records_sources.local_file(filename=mock_filename,
-                                             records_format=mock_records_format) as out:
-            self.assertEqual(type(out), DataUrlRecordsSource)
+        out = self.records_sources.local_file(filename=mock_filename,
+                                              records_format=mock_records_format)
+        self.assertEqual(type(out), DataUrlRecordsSource)
