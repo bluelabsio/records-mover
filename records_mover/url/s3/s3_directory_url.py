76f2d338c12e2d4a359a65564351eab984ce16a1
Vince Broz <vince.broz@bluelabs.com>
2020-05-14 20:33:22 -0400

    Add directories_in_directory() so we can copy records directories from S3

--- records_mover/url/s3/s3_directory_url.py
+++ records_mover/url/s3/s3_directory_url.py
@@ -15,6 +15,13 @@ class S3DirectoryUrl(S3BaseUrl, BaseDirectoryUrl):
         keys = [item['Key'] for item in resp.get('Contents', [])]
         return [self._key_in_same_bucket(key) for key in keys]
 
+    def directories_in_directory(self) -> List['BaseDirectoryUrl']:
+        prefix = self.key
+        response = self.s3_client.list_objects_v2(Bucket=self.bucket, Prefix=prefix, Delimiter='/')
+        prefix_keys = [content['Prefix'] for content in response.get('CommonPrefixes', [])]
+        urls = [f"s3://{self.bucket}/{prefix}/{key}" for key in prefix_keys]
+        return [self._directory(url) for url in urls]
+
     def purge_directory(self) -> None:
         if not self.is_directory():
             raise ValueError("Not a directory")
