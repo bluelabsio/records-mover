0f2c94a9fc9e5a6af8327f937d1f64106566656c
Vince Broz <vince.broz@bluelabs.com>
2020-05-16 16:04:21 -0400

    Fix typing

--- types/stubs/google/cloud/storage/__init__.pyi
+++ types/stubs/google/cloud/storage/__init__.pyi
@@ -1,6 +1,7 @@
 import google.auth.credentials
 from google.cloud.storage.bucket import Bucket
-from typing import Optional
+from google.cloud.storage.blob import Blob
+from typing import Optional, Iterator
 
 
 # https://googleapis.dev/python/storage/latest/client.html
@@ -11,4 +12,10 @@ class Client:
 
     def bucket(self,
                bucket_name: str) -> Bucket:
+        ...
+
+    def list_blobs(self,
+                   bucket_or_name: str,
+                   prefix: Optional[str] = None,
+                   delimiter: Optional[str] = None) -> Iterator[Blob]:
         ...
--- types/stubs/google/cloud/storage/bucket/__init__.py
+++ types/stubs/google/cloud/storage/bucket/__init__.py
@@ -3,11 +3,6 @@ from google.cloud.storage.blob import Blob
 
 
 class Bucket:
-    def list_blobs(self,
-                   prefix: Optional[str] = None,
-                   delimiter: Optional[str] = None) -> Iterator[Blob]:
-        ...
-
     def rename_blob(self, blob: Blob, new_name: str) -> Blob:
         ...
 
