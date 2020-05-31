ce8c7aca45592f811233a6c050d21364920f2a99
Vince Broz <vince.broz@bluelabs.com>
2020-05-31 16:26:39 -0400

    Initial code for BigQuery load from GCS bucket

--- records_mover/db/bigquery/loader.py
+++ records_mover/db/bigquery/loader.py
@@ -11,6 +11,7 @@ from ...records.processing_instructions import ProcessingInstructions
 from ...url.resolver import UrlResolver
 from google.cloud.bigquery.dbapi.connection import Connection
 from google.cloud.bigquery.client import Client
+from google.cloud.bigquery.job import LoadJobConfig
 from .load_job_config_options import load_job_config
 import logging
 from ..loader import LoaderFromFileobj
@@ -40,16 +41,7 @@ class BigQueryLoader(LoaderFromFileobj):
 
         return (project, dataset)
 
-    def load_from_fileobj(self, schema: str, table: str,
-                          load_plan: RecordsLoadPlan, fileobj: IO[bytes]) -> int:
-        # https://googleapis.github.io/google-cloud-python/latest/bigquery/usage/tables.html#creating-a-table
-        connection: Connection =\
-            self.db.engine.raw_connection().connection
-        # https://google-cloud.readthedocs.io/en/latest/bigquery/generated/google.cloud.bigquery.client.Client.html
-        client: Client = connection._client
-        project_id, dataset_id = self._parse_bigquery_schema_name(schema)
-        # https://googleapis.github.io/google-cloud-python/latest/bigquery/generated/google.cloud.bigquery.job.LoadJobConfig.html
-
+    def _load_job_config(self, load_plan: RecordsLoadPlan) -> LoadJobConfig:
         target_records_format = load_plan.records_format
         processing_instructions = load_plan.processing_instructions
         unhandled_hints = set()
@@ -59,6 +51,20 @@ class BigQueryLoader(LoaderFromFileobj):
         if isinstance(target_records_format, DelimitedRecordsFormat):
             complain_on_unhandled_hints(processing_instructions.fail_if_dont_understand,
                                         unhandled_hints, target_records_format.hints)
+        return job_config
+
+    def load_from_fileobj(self, schema: str, table: str,
+                          load_plan: RecordsLoadPlan, fileobj: IO[bytes]) -> int:
+        logger.info("Loading from fileobj into BigQuery")
+        # https://googleapis.github.io/google-cloud-python/latest/bigquery/usage/tables.html#creating-a-table
+        connection: Connection =\
+            self.db.engine.raw_connection().connection
+        # https://google-cloud.readthedocs.io/en/latest/bigquery/generated/google.cloud.bigquery.client.Client.html
+        client: Client = connection._client
+        project_id, dataset_id = self._parse_bigquery_schema_name(schema)
+        # https://googleapis.github.io/google-cloud-python/latest/bigquery/generated/google.cloud.bigquery.job.LoadJobConfig.html
+        job_config = self._load_job_config(load_plan)
+
         logger.info(f"Using BigQuery load options: {job_config.to_api_repr()}")
         # https://googleapis.dev/python/bigquery/latest/generated/google.cloud.bigquery.client.Client.html#google.cloud.bigquery.client.Client.load_table_from_file
         job = client.load_table_from_file(fileobj,
@@ -82,21 +88,32 @@ class BigQueryLoader(LoaderFromFileobj):
              table: str,
              load_plan: RecordsLoadPlan,
              directory: RecordsDirectory) -> int:
+        logger.info("Loading from records directory into BigQuery")
+        # https://googleapis.github.io/google-cloud-python/latest/bigquery/usage/tables.html#creating-a-table
+        connection: Connection =\
+            self.db.engine.raw_connection().connection
+        # https://google-cloud.readthedocs.io/en/latest/bigquery/generated/google.cloud.bigquery.client.Client.html
+        client: Client = connection._client
+        project_id, dataset_id = self._parse_bigquery_schema_name(schema)
+        job_config = self._load_job_config(load_plan)
+
         all_urls = directory.manifest_entry_urls()
 
-        total_rows = 0
+        job = client.load_table_from_uri(all_urls,
+                                         f"{schema}.{table}",
+                                         # Must match the destination dataset location.
+                                         location="US",
+                                         job_config=job_config)
+        try:
+            job.result()  # Waits for table load to complete.
+        except Exception:
+            logger.error(f"BigQuery load errors:\n\n{pprint.pformat(job.errors)}\n")
+            raise
 
-        for url in all_urls:
-            loc = self.url_resolver.file_url(url)
-            with loc.open() as f:
-                # We set WriteDisposition.WRITE_APPEND in
-                # load_options_config.py and let the records Prep
-                # class decide what to do about the existing table, so
-                # it's safe to call this multiple times and append until
-                # done:
-                logger.info(f"Loading {url} into {schema}.{table}")
-                total_rows += self.load_from_fileobj(schema, table, load_plan, f)
-        return total_rows
+        logger.info(f"Loaded {job.output_rows} rows into {dataset_id}:{table}")
+        assert job.output_rows is not None  # should be populated after job result is obtained
+        return job.output_rows
+
 
     def can_load_this_format(self, source_records_format: BaseRecordsFormat) -> bool:
         try:
