a563aa346b020604633939ad0d78e00eae49b1d8
Vince Broz <vince.broz@bluelabs.com>
2020-07-06 13:07:25 -0400

    Use get_args() instead of VALID_VARIANTS

--- tests/unit/records/test_validated_records_hints.py
+++ tests/unit/records/test_validated_records_hints.py
@@ -1,11 +1,13 @@
 import records_mover
+from typing_inspect import get_args
+from records_mover.records.hints import DelimitedVariant
 from records_mover.records import DelimitedRecordsFormat
 import unittest
 
 
 class TestValidatedRecordsHints(unittest.TestCase):
     def test_valid(self):
-        variants = records_mover.records.types.VALID_VARIANTS
+        variants = list(get_args(DelimitedVariant))  # type: ignore
         for variant in variants:
             records_format = DelimitedRecordsFormat(variant=variant)
             records_format.validate(fail_if_cant_handle_hint=True)
