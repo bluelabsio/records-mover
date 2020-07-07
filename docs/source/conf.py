413f5ac51cddf777630721436f5c52efdfb54c27
Vince Broz <vince.broz@bluelabs.com>
2020-07-07 13:13:10 -0400

    Document parameter types in description of parameter, not in function signature part of documentation.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -55,3 +55,11 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# -- Options for type generation ---------------------------------------------
+
+#
+# Document parameter types in description of parameter, not in
+# function signature part of documentation.
+#
+autodoc_typehints = 'description'
