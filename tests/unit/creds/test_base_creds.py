72c2cf27b0826d44ab3517e3a9fd7be27723bb25
vinceatbluelabs <vince.broz@bluelabs.com>
2020-11-11 09:06:57 -0500

    Update tests/unit/creds/test_base_creds.py
    Co-authored-by: Sonja Duric <duric.sonja@gmail.com>

--- tests/unit/creds/test_base_creds.py
+++ tests/unit/creds/test_base_creds.py
@@ -152,7 +152,7 @@ class TestBaseCreds(unittest.TestCase):
 
     @patch('records_mover.creds.base_creds.get_config')
     @patch('records_mover.creds.base_creds.os')
-    def test_gcs_scratch_bucket_not_configured(self,
+    def test_gcs_scratch_bucket_not_configured_true(self,
                                                mock_os,
                                                mock_get_config):
         mock_get_config.return_value.config = {
