0db9b9cde482c662b293d1d894ba1f2b0cf3dd9b
Vince Broz <vince.broz@bluelabs.com>
2020-11-12 07:41:16 -0500

    Move more code into CopyOptimizer class

--- records_mover/url/optimizer/__init__.py
+++ records_mover/url/optimizer/__init__.py
@@ -1,9 +1,18 @@
+import json
+import time
 from contextlib import contextmanager
 from records_mover.url.base import BaseDirectoryUrl
+# TODO: Can I do the following block lazily in case google stuff isn't
+# in environment?
+import google.auth.credentials
+import googleapiclient
 from records_mover.url.gcs.gcs_directory_url import GCSDirectoryUrl
 from records_mover.url.s3.s3_directory_url import S3DirectoryUrl
-from typing import Iterator, Tuple, Union, Optional
+#
 import logging
+from typing import Iterator, Tuple, Union, Optional, TYPE_CHECKING
+if TYPE_CHECKING:
+    from googleapiclient.discovery import _TransferJobConfig
 
 
 logger = logging.getLogger(__name__)
@@ -13,6 +22,123 @@ class CopyOptimizer:
     # TODO: move to new location
     # TODO: Bring in Google code
     # TODO: Figure out where this should be called from
+
+    def copy_via_gcp_data_transfer(self,
+                                   loc: 'S3DirectoryUrl',
+                                   other_loc: 'GCSDirectoryUrl') -> bool:
+        # https://cloud.google.com/storage-transfer/docs/create-manage-transfer-program#python
+
+        if loc.key != other_loc.blob:
+            logger.warning("S3 directory does not match GCS directory - "
+                           "cannot copy S3 bucket using Google Storage Transfer.  "
+                           "Falling back to a slower method of bucket copy.")
+            return False
+
+        gcp_credentials = other_loc.credentials
+        storagetransfer = googleapiclient.discovery.build('storagetransfer', 'v1',
+                                                          credentials=gcp_credentials)
+        description = "records-mover one-time job"
+        # TODO
+        project_id = "bluelabs-tools-dev"
+        # TODO
+        current_day = 10
+        # TODO
+        current_month = 11
+        # TODO
+        current_year = 2020
+        # TODO
+        sink_bucket = "bluelabs-test-recordsmover"
+        source_bucket = loc.bucket
+        aws_creds = loc.aws_creds()
+        if aws_creds is None:
+            logger.warning("S3 bucket did not provide AWS creds - "
+                           "cannot copy S3 bucket using Google Storage Transfer.  "
+                           "Falling back to a slower method of bucket copy.")
+            return False
+        if aws_creds.token is not None:
+            logger.warning("S3 bucket is using a temporary access token (MFA creds?) which "
+                           "Google Storage Transfer does not support.  Falling back to a slower "
+                           "method of bucket copy.")
+            return False
+
+        access_key_id = aws_creds.access_key
+        secret_access_key = aws_creds.secret_key
+
+        # Edit this template with desired parameters.
+        transfer_job: '_TransferJobConfig' = {
+            'description': description,
+            'status': 'ENABLED',
+            'projectId': project_id,
+            'schedule': {
+                'scheduleStartDate': {
+                    'day': current_day,
+                    'month': current_month,
+                    'year': current_year,
+                },
+                'scheduleEndDate': {
+                    'day': current_day,
+                    'month': current_month,
+                    'year': current_year,
+                }
+            },
+            'transferSpec': {
+                'awsS3DataSource': {
+                    'bucketName': source_bucket,
+                    'awsAccessKey': {
+                        'accessKeyId': access_key_id,
+                        'secretAccessKey': secret_access_key
+                    }
+                },
+                "objectConditions": {
+                    "includePrefixes": [
+                        loc.key
+                    ]
+                },
+                "transferOptions": {
+                    "overwriteObjectsAlreadyExistingInSink": False,
+                    "deleteObjectsUniqueInSink": False,
+                    "deleteObjectsFromSourceAfterTransfer": False
+                },
+                'gcsDataSink': {
+                    'bucketName': sink_bucket
+                }
+            }
+        }
+
+        result = storagetransfer.transferJobs().create(body=transfer_job).execute()
+        print('Returned transferJob: {}'.format(
+            json.dumps(result, indent=4)))
+        job_name = result['name']
+        self._wait_for_transfer_job(project_id, job_name, gcp_credentials)
+        return True
+
+    def _wait_for_transfer_job(self,
+                               project_id: str,
+                               job_name: str,
+                               gcp_credentials: google.auth.credentials.Credentials) -> None:
+        # TODO: Pass in correct creds
+        storagetransfer = googleapiclient.discovery.build('storagetransfer',
+                                                          'v1',
+                                                          credentials=gcp_credentials)
+
+        # https://github.com/karthikey-surineni/bigquery-copy-dataset/blob/fb6be5aee9898cba79387a2f95fb54b8b1119a92/src/storage/storage_transfer.py
+        filterString = (
+            '{{"project_id": "{project_id}", '
+            '"job_names": ["{job_name}"]}}'
+        ).format(project_id=project_id, job_name=job_name)
+
+        done = False
+        logger.info("Awaiting completion of Google Storage Transfer Service job...")
+        while not done:
+            result = storagetransfer.transferOperations().list(
+                name="transferOperations",
+                filter=filterString).execute()
+            print('Result of transferOperations/list: {}'.format(
+                json.dumps(result, indent=4, sort_keys=True)))
+            if result != {}:
+                done = result['operations'][0]['metadata']['status'] != 'IN_PROGRESS'
+            time.sleep(10)
+        logger.info("Google Storage Transfer Service job complete.")
 
     def try_swapping_bucket_path(self,
                                  target_bucket: Union[GCSDirectoryUrl, S3DirectoryUrl],
