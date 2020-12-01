f9f88ad74c255892bb7987f11ae3d5a03206fb0d
Vince Broz <vince.broz@bluelabs.com>
2020-12-01 08:48:16 -0500

    Improve test coverage

--- tests/unit/db/redshift/test_unloader.py
+++ tests/unit/db/redshift/test_unloader.py
@@ -35,6 +35,36 @@ class TestRedshiftUnloader(unittest.TestCase):
                                mock_processing_instructions.fail_if_cant_handle_hint)
         self.assertEqual(True, out)
 
+    @patch('records_mover.db.redshift.unloader.redshift_unload_options')
+    @patch('records_mover.db.redshift.unloader.RecordsUnloadPlan')
+    def test_can_unload_format_delimite_false(self,
+                                              mock_RecordsUnloadPlan,
+                                              mock_redshift_unload_options):
+        mock_db = Mock(name='db')
+        mock_table = Mock(name='table')
+
+        mock_target_records_format = Mock(name='target_records_format', spec=DelimitedRecordsFormat)
+        mock_unload_plan = mock_RecordsUnloadPlan.return_value
+        mock_unload_plan.records_format = mock_target_records_format
+
+        mock_processing_instructions = mock_unload_plan.processing_instructions
+        mock_s3_temp_base_loc = Mock(name='s3_temp_base_loc')
+        mock_target_records_format.hints = {}
+        mock_redshift_unload_options.side_effect = NotImplementedError
+
+        redshift_unloader =\
+            RedshiftUnloader(db=mock_db,
+                             table=mock_table,
+                             s3_temp_base_loc=mock_s3_temp_base_loc)
+        out = redshift_unloader.can_unload_format(mock_target_records_format)
+        mock_RecordsUnloadPlan.\
+            assert_called_with(records_format=mock_target_records_format)
+        mock_redshift_unload_options.\
+            assert_called_with(set(),
+                               mock_unload_plan.records_format,
+                               mock_processing_instructions.fail_if_cant_handle_hint)
+        self.assertEqual(False, out)
+
     def test_can_unload_to_scheme_s3_true(self):
         mock_db = Mock(name='db')
         mock_table = Mock(name='table')
