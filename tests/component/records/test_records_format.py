693c84b621b6440594ba9ae29332fc3520160f5f
Vince Broz <vince.broz@bluelabs.com>
2020-12-01 14:57:40 -0500

    Increase coverage

--- tests/component/records/test_records_format.py
+++ tests/component/records/test_records_format.py
@@ -6,3 +6,7 @@ class TestRecordsFormat(unittest.TestCase):
     def test_AvroRecordsFormat_str(self):
         records_format = AvroRecordsFormat()
         self.assertEqual(str(records_format), 'AvroRecordsFormat')
+
+    def test_AvroRecordsFormat_repr(self):
+        records_format = AvroRecordsFormat()
+        self.assertEqual(repr(records_format), 'AvroRecordsFormat')
