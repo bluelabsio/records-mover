5ebb19b8540aeb68b3737a5527a160c2c3c25a51
Vince Broz <vince.broz@bluelabs.com>
2020-12-01 08:40:04 -0500

    Improve test coverage

--- tests/unit/db/bigquery/test_bigquery_loader.py
+++ tests/unit/db/bigquery/test_bigquery_loader.py
@@ -2,11 +2,16 @@ import unittest
 
 from records_mover.db.bigquery.loader import BigQueryLoader
 from records_mover.records.records_format import (
-    DelimitedRecordsFormat, ParquetRecordsFormat, AvroRecordsFormat
+    DelimitedRecordsFormat, ParquetRecordsFormat, AvroRecordsFormat,
+    BaseRecordsFormat
 )
 from records_mover.db.errors import NoTemporaryBucketConfiguration
 from mock import MagicMock, Mock
 from unittest.mock import patch
+
+
+class NewRecordsFormat(BaseRecordsFormat):
+    ...
 
 
 class TestBigQueryLoader(unittest.TestCase):
@@ -253,6 +258,31 @@ class TestBigQueryLoader(unittest.TestCase):
             assert_called_with(records_format=mock_source_records_format,
                                processing_instructions=mock_processing_instructions)
         self.assertTrue(out)
+
+    @patch('records_mover.db.bigquery.loader.load_job_config')
+    @patch('records_mover.db.bigquery.loader.ProcessingInstructions')
+    @patch('records_mover.db.bigquery.loader.RecordsLoadPlan')
+    def test_can_load_this_format_false_newformat(self,
+                                                  mock_RecordsLoadPlan,
+                                                  mock_ProcessingInstructions,
+                                                  mock_load_job_config):
+        mock_db = Mock(name='db')
+        mock_source_records_format = Mock(name='source_records_format', spec=NewRecordsFormat)
+        mock_source_records_format.format_type = 'new'
+        mock_processing_instructions = mock_ProcessingInstructions.return_value
+        mock_load_plan = mock_RecordsLoadPlan.return_value
+        mock_load_plan.records_format = mock_source_records_format
+        mock_url_resolver = Mock(name='url_resolver')
+        mock_source_records_format.hints = {}
+        bigquery_loader = BigQueryLoader(db=mock_db, url_resolver=mock_url_resolver,
+                                         gcs_temp_base_loc=None)
+        out = bigquery_loader.can_load_this_format(mock_source_records_format)
+        mock_ProcessingInstructions.assert_called_with()
+        mock_RecordsLoadPlan.\
+            assert_called_with(records_format=mock_source_records_format,
+                               processing_instructions=mock_processing_instructions)
+        self.assertFalse(out)
+
 
     def test_known_supported_records_formats_for_load(self):
         mock_db = Mock(name='db')
