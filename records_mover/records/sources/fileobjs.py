cc276350feda4e7b678d6898a1cb165321a385ca
Vince Broz <vince.broz@bluelabs.com>
2020-12-03 09:01:28 -0500

    Swap out to generator expression using common function

--- records_mover/records/sources/fileobjs.py
+++ records_mover/records/sources/fileobjs.py
@@ -13,13 +13,13 @@ from ..processing_instructions import ProcessingInstructions
 from ...records.delimited import complain_on_unhandled_hints
 from ..delimited import python_encoding_from_hint
 from ..schema import RecordsSchema
+from records_mover.pandas import convert_integer_dtypes
 from records_mover.url.filesystem import FilesystemDirectoryUrl
 from records_mover.url.base import BaseDirectoryUrl
 import logging
 from typing import Mapping, IO, Optional, Iterator, List, Any, TYPE_CHECKING
 if TYPE_CHECKING:
     from .dataframes import DataframesRecordsSource  # noqa
-    from pandas import Dataframe
 
 
 logger = logging.getLogger(__name__)
@@ -155,19 +155,6 @@ class FileobjsSource(SupportsMoveToRecordsDirectory,
             else:
                 chunksize = int(entries_per_chunk / num_fields)
 
-            def fix_integer_columns(dfs: Iterator['Dataframe']) -> Iterator['Dataframe']:
-                # TODO: Move generator expression using that function
-                for df in dfs:
-                    if 'convert_dtypes' in dir(df):
-                        # Allow nullable integers to be represented
-                        df = df.convert_dtypes(convert_integer=True)
-                    else:
-                        logger.warning("Using old version of pandas; "
-                                       "not able to represent nullable integer columns")
-                    yield df
-
-            # TODO: switch to generator expressions: https://treyhunner.com/2018/06/how-to-make-an-iterator-in-python/
-
             try:
                 dfs = pd.read_csv(filepath_or_buffer=target_fileobj,
                                   iterator=True,
@@ -175,7 +162,7 @@ class FileobjsSource(SupportsMoveToRecordsDirectory,
                                   **options)
             except pd.errors.EmptyDataError:
                 dfs = [self.records_schema.to_empty_dataframe()]
-            yield DataframesRecordsSource(dfs=fix_integer_columns(dfs),
+            yield DataframesRecordsSource(dfs=(convert_integer_dtypes(df) for df in dfs),
                                           records_schema=self.records_schema)
         finally:
             if text_fileobj is not None:
