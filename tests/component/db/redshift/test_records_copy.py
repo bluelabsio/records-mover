b16bda26bad0e77db7c5811734fed900b65c19e8
Vince Broz <vince.broz@bluelabs.com>
2020-12-04 13:34:41 -0500

    Add test_redshift_copy_options_dateformat

--- tests/component/db/redshift/test_records_copy.py
+++ tests/component/db/redshift/test_records_copy.py
@@ -2,6 +2,7 @@ import unittest
 from records_mover.db.redshift.records_copy import redshift_copy_options
 from records_mover.records import DelimitedRecordsFormat, AvroRecordsFormat
 from sqlalchemy_redshift.commands import Encoding, Format
+from ..records.datetime_cases import DATE_CASES
 
 
 class TestRecordsCopy(unittest.TestCase):
@@ -35,3 +36,18 @@ class TestRecordsCopy(unittest.TestCase):
                                     fail_if_row_invalid=True,
                                     max_failure_rows=0)
         self.assertIs(out['format'], Format.avro)
+
+    def test_redshift_copy_options_dateformat(self):
+        for dateformat in DATE_CASES:
+            records_format =\
+                DelimitedRecordsFormat(variant='bluelabs',
+                                       hints={
+                                           'dateformat': dateformat
+                                       })
+            unhandled_hints = set(records_format.hints.keys())
+            out = redshift_copy_options(unhandled_hints,
+                                        records_format,
+                                        fail_if_cant_handle_hint=True,
+                                        fail_if_row_invalid=True,
+                                        max_failure_rows=0)
+            self.assertIs(out['date_format'], 123)
