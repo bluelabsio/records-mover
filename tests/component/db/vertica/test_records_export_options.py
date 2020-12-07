95ae132eb10dbcf648c415f3bf74cc43a8cca887
Vince Broz <vince.broz@bluelabs.com>
2020-12-07 12:01:59 -0500

    More testing

--- tests/component/db/vertica/test_records_export_options.py
+++ tests/component/db/vertica/test_records_export_options.py
@@ -3,14 +3,18 @@ from records_mover.records.load_plan import RecordsLoadPlan
 from records_mover.db.vertica.records_export_options import vertica_export_options
 from records_mover.records import DelimitedRecordsFormat, ProcessingInstructions
 from ...records.datetime_cases import (
-    DATE_CASES, create_sample, SAMPLE_YEAR, SAMPLE_MONTH, SAMPLE_DAY
+    DATE_CASES, DATETIMEFORMATTZ_CASES,
+    create_sample, SAMPLE_YEAR, SAMPLE_MONTH, SAMPLE_DAY
 )
 
 
 class TestVerticaExportOptions(unittest.TestCase):
     maxDiff = None
 
-    def test_vertica_export_options_datetime(self):
+    def test_vertica_export_options_dateformat(self):
+        # Vertica doesn't currently allow any configurability on
+        # output dateformat.  Check again before adding any test
+        # cases here!
         should_raise = {
             'YYYY-MM-DD': False,
             'MM-DD-YYYY': True,
@@ -34,6 +38,34 @@ class TestVerticaExportOptions(unittest.TestCase):
                 vertica_export_options(unhandled_hints, load_plan)
             except NotImplementedError:
                 if should_raise[dateformat]:
+                    pass
+                else:
+                    self.fail()
+
+    def test_vertica_export_options_datetimeformattz(self):
+        # Vertica doesn't currently allow any configurability on
+        # output datetimeformattz.  Check again before adding any test
+        # cases here!
+        should_raise = {
+            'YYYY-MM-DD HH:MI:SS': True,
+            'YYYY-MM-DD HH24:MI:SSOF': False,
+            'MM/DD/YY HH24:MI': True,
+        }
+        for datetimeformattz in DATETIMEFORMATTZ_CASES:
+            records_format = DelimitedRecordsFormat(variant='vertica',
+                                                    hints={
+                                                        'datetimeformattz': datetimeformattz
+                                                    })
+            unhandled_hints = set(records_format.hints)
+            processing_instructions = ProcessingInstructions(max_failure_rows=123)
+            load_plan = RecordsLoadPlan(processing_instructions=processing_instructions,
+                                        records_format=records_format)
+            # Records Mover passes no particular option for dateformat on
+            # export in Vertica; it always uses YYYY-MM-DD as a result.
+            try:
+                vertica_export_options(unhandled_hints, load_plan)
+            except NotImplementedError:
+                if should_raise[datetimeformattz]:
                     pass
                 else:
                     self.fail()
