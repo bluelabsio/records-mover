7df3342b4e4b1e4f929abbe291c6c5537b345754
Vince Broz <vince.broz@bluelabs.com>
2020-12-08 16:12:53 -0500

    Add TODOs

--- records_mover/records/pandas/to_csv_options.py
+++ records_mover/records/pandas/to_csv_options.py
@@ -63,8 +63,13 @@ def pandas_to_csv_options(records_format: DelimitedRecordsFormat,
     pandas_options['header'] = hints.header_row
     quiet_remove(unhandled_hints, 'header-row')
 
+    # TODO: Does it make sense to require dateformat to be consistent
+    # with datetimeformat{tz}?  Looks ike prep_for_csv now preformats that.
     if hints.dateformat is None:
         if hints.datetimeformattz == hints.datetimeformat:
+            # TODO: This comment isn't related here and should be
+            # moved and replaced with one that explains the == above
+            #
             # BigQuery requires that timezone offsets have a colon;
             # Python (and thus Pandas) doesn't support adding the
             # colon with strftime.  However, we can specify things
