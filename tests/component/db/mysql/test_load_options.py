c94b66dba5f0a6a609be7c50aa63f21be513e006
vinceatbluelabs <vince.broz@bluelabs.com>
2020-12-10 11:45:01 -0500

    Improve date/time hint handling for MySQL based on better testing (#151)
    * Improve date/time hint handling for MySQL based on better testing
    
    Based on the new testing approach in #144, this PR adds component
    tests for date/time types while loading CSV files in MySQL.
    
    The expectations for those test are based on the results of the
    integration tests in #140, which shows our MySQL driver is way too
    optimistic about what date/time hints it can import!
    
    * Make comment in complete sentences
    
    * Quick flake8 fix
    
    * csv variant proved to not be possible
    
    * Ratchet
    
    * Ratchet

--- tests/component/db/mysql/test_load_options.py
+++ tests/component/db/mysql/test_load_options.py
@@ -1,6 +1,10 @@
 import unittest
 from records_mover.records import DelimitedRecordsFormat
 from records_mover.db.mysql.load_options import mysql_load_options
+from ...records.datetime_cases import (
+    DATE_CASES, DATETIMETZ_CASES, DATETIME_CASES, TIMEONLY_CASES,
+)
+from typing import Set
 
 
 class TestMySQLLoadOptions(unittest.TestCase):
@@ -99,3 +103,100 @@ class TestMySQLLoadOptions(unittest.TestCase):
                                fail_if_cant_handle_hint=True)
 
         self.assertIn("doublequote='mumble'", str(r.exception))
+
+    def test_mysql_load_options_dateformat(self) -> None:
+        expected_failures: Set[str] = {
+            'MM-DD-YYYY',
+            'DD-MM-YYYY',
+            'MM/DD/YY',
+            'DD/MM/YY',
+            'DD-MM-YY',
+        }
+
+        for dateformat in DATE_CASES:
+            records_format = DelimitedRecordsFormat(variant='bluelabs',
+                                                    hints={
+                                                        'dateformat': dateformat,
+                                                        'compression': None,
+                                                    })
+            unhandled_hints = set(records_format.hints.keys())
+            try:
+                mysql_load_options(unhandled_hints,
+                                   records_format,
+                                   fail_if_cant_handle_hint=True)
+            except NotImplementedError:
+                if dateformat in expected_failures:
+                    continue
+                else:
+                    raise
+            self.assertNotIn(dateformat, expected_failures)
+
+    def test_mysql_load_options_datetimeformattz(self) -> None:
+        expected_failures: Set[str] = {
+            'MM/DD/YY HH24:MI'
+        }
+
+        for datetimeformattz in DATETIMETZ_CASES:
+            records_format = DelimitedRecordsFormat(variant='bluelabs',
+                                                    hints={
+                                                        'datetimeformattz': datetimeformattz,
+                                                        'compression': None,
+                                                    })
+            unhandled_hints = set(records_format.hints.keys())
+            try:
+                mysql_load_options(unhandled_hints,
+                                   records_format,
+                                   fail_if_cant_handle_hint=True)
+            except NotImplementedError:
+                if datetimeformattz in expected_failures:
+                    continue
+                else:
+                    raise
+            self.assertNotIn(datetimeformattz, expected_failures)
+
+    def test_mysql_load_options_datetimeformat(self) -> None:
+        expected_failures: Set[str] = {
+            'YYYY-MM-DD HH12:MI AM',
+            'MM/DD/YY HH24:MI',
+        }
+
+        for datetimeformat in DATETIME_CASES:
+            records_format = DelimitedRecordsFormat(variant='bluelabs',
+                                                    hints={
+                                                        'datetimeformat': datetimeformat,
+                                                        'compression': None,
+                                                    })
+            unhandled_hints = set(records_format.hints.keys())
+            try:
+                mysql_load_options(unhandled_hints,
+                                   records_format,
+                                   fail_if_cant_handle_hint=True)
+            except NotImplementedError:
+                if datetimeformat in expected_failures:
+                    continue
+                else:
+                    raise
+            self.assertNotIn(datetimeformat, expected_failures)
+
+    def test_mysql_load_options_timeonlyformat(self) -> None:
+        expected_failures: Set[str] = {
+            'HH12:MI AM',
+        }
+
+        for timeonlyformat in TIMEONLY_CASES:
+            records_format = DelimitedRecordsFormat(variant='bluelabs',
+                                                    hints={
+                                                        'timeonlyformat': timeonlyformat,
+                                                        'compression': None,
+                                                    })
+            unhandled_hints = set(records_format.hints.keys())
+            try:
+                mysql_load_options(unhandled_hints,
+                                   records_format,
+                                   fail_if_cant_handle_hint=True)
+            except NotImplementedError:
+                if timeonlyformat in expected_failures:
+                    continue
+                else:
+                    raise
+            self.assertNotIn(timeonlyformat, expected_failures)
