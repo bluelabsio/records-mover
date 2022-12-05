a49c7249cd79d99ed78da4fe17eedd4213bba1bb
Tim Ryan <tim.ryan@bluelabs.com>
2022-12-05 17:52:25 -0500

    add type ignore

--- records_mover/mover_types.py
+++ records_mover/mover_types.py
@@ -1,4 +1,3 @@
-# type: ignore
 from enum import Enum
 from typing import Dict, Any, Optional, Union, Mapping, List, NoReturn
 
@@ -20,16 +19,18 @@ def _assert_never(x: NoReturn, errmsg: Optional[str] = None) -> NoReturn:
     assert False, errmsg
 
 
-# mypy way of validating we're covering all cases of an enum.  This
-# version allows poorly typed things to pass through at runtime.
-#
-# https://github.com/python/mypy/issues/6366#issuecomment-560369716
-def _ensure_all_cases_covered(x: NoReturn) -> NoReturn:
-    pass
-
-
 # mypy-friendly way of doing a singleton object:
 #
 # https://github.com/python/typing/issues/236
 class PleaseInfer(Enum):
     token = 1
+
+
+# mypy way of validating we're covering all cases of an enum.  This
+# version allows poorly typed things to pass through at runtime.
+#
+# https://github.com/python/mypy/issues/6366#issuecomment-560369716
+# type: ignore
+def _ensure_all_cases_covered(x: NoReturn) -> NoReturn:
+    pass
+
