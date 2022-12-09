f3aabda19cf5d35566754edcff1b5f586bd0604a
Tim Ryan <tim.ryan@bluelabs.com>
2022-12-09 11:49:15 -0500

    add type ignore to google cloud storage bucket

--- types/stubs/google/cloud/storage/bucket/__init__.py
+++ types/stubs/google/cloud/storage/bucket/__init__.py
@@ -2,8 +2,8 @@ from google.cloud.storage.blob import Blob
 
 
 class Bucket:
-    def rename_blob(self, blob: Blob, new_name: str) -> Blob:
+    def rename_blob(self, blob: Blob, new_name: str) -> Blob:  # type: ignore
         ...
 
-    def blob(self, blob_name: str) -> Blob:
+    def blob(self, blob_name: str) -> Blob:  # type: ignore
         ...
