9796bb7eb8820699e28ae758334ae82b854f8e97
Tim Ryan <ryantimjohn@gmail.com>
2022-12-14 09:06:26 -0500

    add pytz to itest dependencies

--- setup.py
+++ setup.py
@@ -126,6 +126,7 @@ nose_dependencies = [
 
 itest_dependencies = [
     'jsonschema',  # needed for directory_validator.py
+    'pytz',
 ] + (
     nose_dependencies +
     # needed for records_database_fixture retrying drop/creates on
