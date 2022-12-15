d2a4563efd1c991e35c5ee3559460cd5e3c07a31
Tim Ryan <tim.ryan@bluelabs.com>
2022-12-15 09:13:20 -0500

    use .item() method

--- records_mover/records/targets/google_sheets.py
+++ records_mover/records/targets/google_sheets.py
@@ -104,7 +104,7 @@ class GoogleSheetsRecordsTarget(SupportsMoveFromDataframes):
     def as_json_serializable(self, cell: Any) -> Any:
         if isinstance(cell, np.generic):
             # MyPy complains that this method does not exist
-            native = np.ndarray.item(cell)  # type: ignore
+            native = cell.item()  # type: ignore
         else:
             native = cell
         if isinstance(cell, float) and math.isnan(native):
