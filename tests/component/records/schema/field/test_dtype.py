3be0b11b6251c1a30df41a2271ebef635587a259
“Tim <ryantimjohn@gmail.com>
2023-01-11 15:19:25 +0000

    update to modern pands datatypes

--- tests/component/records/schema/field/test_dtype.py
+++ tests/component/records/schema/field/test_dtype.py
@@ -33,16 +33,16 @@ def check_dtype(field_type, constraints, expectation):
 
 class Test_to_pandas_dtype_integer_no_nullable:
     expectations = {
-        (-100, 100): np.int8,
-        (0, 240): np.uint8,
-        (-10000, 10000): np.int16,
-        (500, 40000): np.uint16,
-        (-200000000, 200000000): np.int32,
-        (25, 4000000000): np.uint32,
-        (-9000000000000000000, 2000000000): np.int64,
-        (25, 10000000000000000000): np.uint64,
+        (-100, 100): pd.Int8Dtype(),
+        (0, 240): pd.UInt8Dtype(),
+        (-10000, 10000): pd.Int16Dtype(),
+        (500, 40000): pd.UInt16Dtype(),
+        (-200000000, 200000000): pd.Int32Dtype(),
+        (25, 4000000000): pd.UInt32Dtype(),
+        (-9000000000000000000, 2000000000): pd.Int64Dtype(),
+        (25, 10000000000000000000): pd.UInt64Dtype(),
         (25, 1000000000000000000000000000): np.float128,
-        (None, None): np.int64,
+        (None, None): pd.Int64Dtype(),
     }
     @pytest.mark.parametrize("expectation", expectations.items())
     def test_to_pandas_dtype_integer_no_nullable(self, expectation):
