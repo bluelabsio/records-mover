b0c3af6d9c8b64669be679e1f0513eeaacb314db
Tim Ryan <tim.ryan@bluelabs.com>
2023-01-12 10:16:12 -0500

    RM-34 update to base_aws syntax

--- tests/unit/test_records_hook.py
+++ tests/unit/test_records_hook.py
@@ -12,7 +12,7 @@ class TestRecordsHook(unittest.TestCase):
     @patch('records_mover.airflow.hooks.records_hook.Records')
     @patch('records_mover.airflow.hooks.records_hook.db_driver')
     @patch('records_mover.airflow.hooks.records_hook.UrlResolver')
-    @patch('records_mover.airflow.hooks.records_hook.base_aws.AwsBaseHook')
+    @patch('records_mover.airflow.providers.amazon.aws.hooks.base_aws.AwsBaseHook')
     def test_get_conn(self,
                       mock_AwsBaseHook,
                       mock_UrlResolver,
