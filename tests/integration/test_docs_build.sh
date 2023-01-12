7e95e38decbe0331733a8be4f88ab8c6226115cf
Bruno Castro-Karney <bruno.castrokarney@bluelabs.com>
2023-01-12 15:29:06 -0800

    Add a locally runnable test to validate readthedocs build
    This isn't perfect but since readthedocs doesn't provide an easy way
    of reproducing their builds, this was my best guess at imitating what
    is happening.

---
+++ tests/integration/test_docs_build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -x
+export PYENV_VERSION=3.8.16
+
+TEST_VENV_NAME=test-docs-sphinx-build
+SCRIPT_DIR=$(dirname $0)
+DOCS_DIR=${SCRIPT_DIR}/../../docs
+
+pyenv install -s ${PYENV_VERSION}
+
+pyenv virtualenv-delete -f ${TEST_VENV_NAME}
+pyenv virtualenv ${TEST_VENV_NAME}
+. ${PYENV_ROOT}/versions/${TEST_VENV_NAME}/bin/activate
+
+pip install -r ${DOCS_DIR}/source/requirements.txt
+
+cd ${DOCS_DIR}
+make clean
+make html
+
+unset PYENV_VERSION
