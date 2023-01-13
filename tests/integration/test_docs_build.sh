51139b2340c9330db97427721af4285824c5bb5f
Bruno Castro-Karney <bruno.castrokarney@bluelabs.com>
2023-01-13 10:38:14 -0800

    Allow test_docs_build.sh to run without creating a virtualenv in CI

--- tests/integration/test_docs_build.sh
+++ tests/integration/test_docs_build.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
-set -x
-export PYENV_VERSION=3.8.16
 
-TEST_VENV_NAME=test-docs-sphinx-build
+if [[ $1 == "--help" ]]
+then
+    echo "Usage: ./test_docs_build.sh [--no-pyenv]"
+    echo "If --no-pyenv is given, no virtualenv will be created to hold "
+    echo "installed packages."
+    exit 0
+elif [[ $1 != "--no-pyenv" ]]
+then
+    echo "Creating virtualenv..."
+    export PYENV_VERSION=3.8.16
+
+    TEST_VENV_NAME=test-docs-sphinx-build
+
+    pyenv install -s ${PYENV_VERSION}
+
+    pyenv virtualenv-delete -f ${TEST_VENV_NAME}
+    pyenv virtualenv ${TEST_VENV_NAME}
+    . ${PYENV_ROOT}/versions/${TEST_VENV_NAME}/bin/activate
+fi
+
+echo "Installing sphinx dependencies..."
 SCRIPT_DIR=$(dirname $0)
 DOCS_DIR=${SCRIPT_DIR}/../../docs
-
-pyenv install -s ${PYENV_VERSION}
-
-pyenv virtualenv-delete -f ${TEST_VENV_NAME}
-pyenv virtualenv ${TEST_VENV_NAME}
-. ${PYENV_ROOT}/versions/${TEST_VENV_NAME}/bin/activate
 
 pip install -r ${DOCS_DIR}/source/sphinx_requirements.txt
 
 cd ${DOCS_DIR}
 make clean
 make html
-
-unset PYENV_VERSION
