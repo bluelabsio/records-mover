ee3a5ad5e8663637d879c50b35818cd85f392437
Tim Ryan <tim.ryan@bluelabs.com>
2023-01-14 11:48:45 -0500

    RM-34 match column widths exp column types

--- tests/integration/records/expected_column_types.py
+++ tests/integration/records/expected_column_types.py
@@ -12,7 +12,8 @@ expected_single_database_column_types = {
         'TIMESTAMP', 'TIMESTAMPTZ'
     ],
     'postgresql': [
-        'INTEGER', 'VARCHAR(3)', 'VARCHAR(3)', 'VARCHAR(1)', 'VARCHAR(1)', 'VARCHAR(3)', 'VARCHAR(111)', 'DATE', 'TIME', 'TIMESTAMP', 'TIMESTAMP'
+        'INTEGER', 'VARCHAR(3)', 'VARCHAR(3)', 'VARCHAR(1)', 'VARCHAR(1)', 
+        'VARCHAR(3)', 'VARCHAR(111)', 'DATE', 'TIME', 'TIMESTAMP', 'TIMESTAMP'
     ],
     'bigquery': [
         'INTEGER', 'VARCHAR(3)', 'VARCHAR(3)', 'VARCHAR(1)', 'VARCHAR(1)', 'VARCHAR(3)',
@@ -26,7 +27,8 @@ expected_single_database_column_types = {
 
 expected_df_loaded_database_column_types = {
     'postgresql': [
-        'BIGINT', 'VARCHAR(12)', 'VARCHAR(12)', 'VARCHAR(4)', 'VARCHAR(4)', 'VARCHAR(12)', 'VARCHAR(444)', 'DATE', 'TIME', 'TIMESTAMP', 'TIMESTAMP'
+        'BIGINT', 'VARCHAR(12)', 'VARCHAR(12)', 'VARCHAR(4)', 'VARCHAR(4)', 'VARCHAR(12)', 
+        'VARCHAR(444)', 'DATE', 'TIME', 'TIMESTAMP', 'TIMESTAMP'
     ],
     'mysql': [
         'BIGINT(20)', 'VARCHAR(3)', 'VARCHAR(3)', 'VARCHAR(1)', 'VARCHAR(1)', 'VARCHAR(3)',
