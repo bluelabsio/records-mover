51f069aef5c9ef94981db4673306a1842e1a290a
Tim Ryan <tim.ryan@bluelabs.com>
2023-07-17 15:06:15 -0400

    Revert "RM-96 use db_conn and db_engine"
    This reverts commit 6a9f5d24cc0f598c12ffa5dca8e701c3d4ed5493.

--- tests/unit/records/test_prep.py
+++ tests/unit/records/test_prep.py
@@ -14,8 +14,7 @@ class TestPrep(unittest.TestCase):
     def test_prep_table_exists_append_implicit(self, mock_quote_schema_and_table):
         mock_schema_sql = Mock(name='schema_sql')
         mock_driver = Mock(name='driver', spec=DBDriver)
-        mock_driver.db_conn = Mock(name='db')
-        mock_driver.db_engine = Mock(name='db_engine')
+        mock_driver.db = Mock(name='db')
 
         mock_driver.has_table.return_value = True
         how_to_prep = ExistingTableHandling.APPEND
@@ -27,8 +26,7 @@ class TestPrep(unittest.TestCase):
     def test_prep_table_exists_truncate_implicit(self, mock_quote_schema_and_table):
         mock_schema_sql = Mock(name='schema_sql')
         mock_driver = Mock(name='driver', spec=DBDriver)
-        mock_driver.db_conn = Mock(name='db')
-        mock_driver.db_engine = Mock(name='db_engine')
+        mock_driver.db = Mock(name='db')
 
         mock_quote_schema_and_table
         mock_driver.has_table.return_value = True
@@ -47,8 +45,7 @@ class TestPrep(unittest.TestCase):
     def test_prep_table_exists_delete_implicit(self, mock_quote_schema_and_table):
         mock_schema_sql = Mock(name='schema_sql')
         mock_driver = Mock(name='driver', spec=DBDriver)
-        mock_driver.db_conn = Mock(name='db')
-        mock_driver.db_engine = Mock(name='db_engine')
+        mock_driver.db = Mock(name='db')
 
         mock_quote_schema_and_table
         mock_driver.has_table.return_value = True
